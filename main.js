6524b8821851b975a152441dafe7ced0b1fdaf4a
italo Vinicius <italorodrigues476@icloud.com>
2024-02-17 21:51:10 +0000

    commit initial

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const Menu = document.querySelector(".menu");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+    navMenu.classList.toggle("ativo");
+});
